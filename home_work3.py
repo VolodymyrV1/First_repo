f84cd2b50f2b8a026d86ad8f6e4a977ac8bf4479
VolodymyrV1 <kotsiubkov@gmail.com>
2024-04-28 23:53:22 +0300

    HW3_corect

--- home_work3.py
+++ home_work3.py
@@ -4,18 +4,13 @@ import random
 #Exs_1
 
 def get_days_from_today(date):
-    date_format = datetime.strptime(date, '%Y-%m-%d')
-    current_day = datetime.today()
-    delta_days = (date_format - current_day).days
-    if delta_days <= 0:
-        delta_days_modul = 0
-        delta_days_modul += abs(delta_days)
-        print(f'This date was {delta_days_modul} days ago.')
-    else:
-        print(f'This date will be in {delta_days} days.')
-
+    date_format = datetime.strptime(date, '%Y-%m-%d').date()
+    today_date = datetime.today().date()
+    delta_days = (date_format - today_date).days
+    return delta_days
+    
 date = input('Enter your date YYYY-MM-DD: ')
-get_days_from_today(date)
+print(get_days_from_today(date))
 
 ''''''
 print('-' *60) #split
@@ -23,15 +18,18 @@ print('-' *60) #split
 #Exs_2
 
 def get_number_random(x, y, v):
-    a = []
-    range_list = list(range(x, y))
-    a += range_list
-    winer_numbers = random.sample(a, k=6)
-    print(f'Winners are people who have numbers {sorted(winer_numbers)}.')
+    list_numbers = []
+    try:
+        range_list = list(range(x, y))
+        list_numbers += range_list
+        winer_numbers = sorted(random.sample(list_numbers, k=v))
+        return winer_numbers
+    except ValueError:
+        return ('Wrong corect numbers.')
 
 x = 1
 y = 50
 v = 6
-get_number_random(x, y, v)
+print(get_number_random(x, y, v))
 
 
